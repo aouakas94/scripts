37181e2dace6a69d9bc1132eee3df5d53bdead08
aouakas94 <cherifiaouakas8994@gmail.com>
2021-05-20 16:09:51 +0200

    hosts

---
+++ hosts.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+gethosts()
+{
+
+liste=()
+while IFS=$'\n' read hosts  ;do
+liste+="${hosts}"
+
+done < hosts
+
+echo ${liste[*]}
+
+}
+
+for i in $(gethosts); do
+   if ! [[ $# = 1  ]]; then
+   echo "veuillez sisair la commande a executer please!"
+   exit 
+
+   fi
+   echo "     "
+	ping -q -c1 $i > /dev/null
+   if [[ $? -eq 1 ]] ; then
+   echo "-------------- pas de connection avec $i  ------------- "
+             echo "      "
+   else
+echo "------la liste des utilisateurs conecté sur $i --------------" ;
+           echo $1 | ssh $i 
+
+    #echo   `ssh $i "$1" | grep pts`
+
+             echo "   "
+     fi
+done
