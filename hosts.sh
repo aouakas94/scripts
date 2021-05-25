36445db77a5d60692c6771cbda9f034bfd53df8d
aouakas94 <cherifiaouakas8994@gmail.com>
2021-05-25 16:16:59 +0200

    Revert "hosts"
    This reverts commit 29eb80437264b6d6507f666f43f53067f8e28639.

--- hosts.sh
+++ hosts.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
-#je declare une vide
 liste=()
-#la liste s'actulise avec l contenu du fichier hosts
 liste=`cat /home/aouakas/Bureau/scripts/hosts`
 
-#pour chaque element de la liste
+
 for i in $liste;do
 
-# verifier le premier parametre est sisaie
    if ! [[ $# = 1  ]]; then
    echo "veuillez sisair la commande a executer please!"
    exit 
 
    fi
    echo "     "
-#faire un ping sur chaqune des adresse ip de liste recuperé dans le fichier hosts
 	ping -q -c1 $i > /dev/null
    if [[ $? -eq 1 ]] ; then
    echo "-------------- pas de connection avec $i  ------------- "
              echo "      "
    else
-#si non si la connexion est etablie , executer la commande
 echo "------la liste des utilisateurs conecté sur $i --------------" ;
            echo $1 | ssh $i 
+
+    #echo   `ssh $i "$1" | grep pts`
+
              echo "   "
      fi
 done
